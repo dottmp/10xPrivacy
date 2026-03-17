e9e5a47e97cd7dd6663f546624f738263fb435e6
jonathan.md <github.com.starship007@passmail.net>
2026-03-17 20:08:40 +0100

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -7,16 +7,19 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
+import unusedImports from 'eslint-plugin-unused-imports';
+import checkFilePlugin from 'eslint-plugin-check-file';
+import importPlugin from 'eslint-plugin-import';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
 export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
-	ts.configs.recommended,
-	svelte.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
 	prettier,
-	svelte.configs.prettier,
+	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
@@ -25,6 +28,54 @@ export default defineConfig(
 			'no-undef': 'off'
 		}
 	},
+
+	//----------------------------------------------------------------------
+	// general
+	//----------------------------------------------------------------------
+	{
+		files: ['**/*.ts', '**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		plugins: { 'unused-imports': unusedImports, import: importPlugin },
+		rules: {
+			'unused-imports/no-unused-imports': 'warn',
+			'import/no-cycle': ['error', { ignoreExternal: true }],
+			'import/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+					'newlines-between': 'always',
+					alphabetize: { order: 'asc', caseInsensitive: true }
+				}
+			]
+		}
+	},
+
+	//----------------------------------------------------------------------
+	// file naming convention
+	//----------------------------------------------------------------------
+	{
+		files: ['**/*.ts', '**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		ignores: ['src/routes/**/*.{ts,svelte}'],
+		plugins: {
+			'check-file': checkFilePlugin
+		},
+		rules: {
+			'check-file/filename-naming-convention': [
+				'error',
+				{
+					'**/*.{ts,svelte}': 'KEBAB_CASE'
+				},
+				{ ignoreMiddleExtensions: true }
+			],
+			'check-file/folder-naming-convention': [
+				'error',
+				{
+					'!(src/routes)/**/*': 'KEBAB_CASE',
+					'!(**/__tests__)/**/*': 'KEBAB_CASE'
+				}
+			]
+		}
+	},
+
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
