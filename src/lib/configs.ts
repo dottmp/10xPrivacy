fca51d4bb8ba8d519e7edd6ed8fe9d6de60da5d7
jonathan <github.com.starship007@passmail.net>
2026-03-22 15:46:21 +0100

    move website config

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -20,3 +20,40 @@ export const SOURCE_REGISTRY = Object.freeze([
 		feedUrl: 'https://techlore.tech/rss/'
 	}
 ] as const satisfies Source[]);
+
+export const FEATURED_CATEGORIES = Object.freeze([
+	'Essentials',
+	'Communication',
+	'Networking',
+	'Security Tools',
+	'Operating Systems',
+	'Productivity'
+]);
+
+export const WEBSITES = Object.freeze([
+	'https://theyseeyourphotos.com',
+	'https://privacyguides.org',
+	'https://techlore.tech',
+	'https://reddit.com/r/privacy',
+	'https://awesome-privacy.xyz',
+	'https://tuta.com/blog',
+	'https://privacytools.io',
+	'https://github.com/brandonhimpfen/awesome-privacy',
+	'https://mullvad.net/en/why-privacy-matters',
+	'https://privacypack.org',
+	'https://www.poisonfountain.org',
+	'https://rnsaffn.com/poison3',
+	'https://haveibeenpwned.com',
+	'https://prism-break.org',
+	'https://github.com/pluja/awesome-privacy',
+	'https://ssd.eff.org',
+	'https://reddit.com/r/thehatedone',
+	'https://browserleaks.com/',
+	'https://mullvad.net/en/servers',
+	'https://protonvpn.com/vpn-servers',
+	'https://haveibeenpwned.com/Passwords',
+	'https://discuss.privacyguides.net',
+	'https://coveryourtracks.eff.org',
+	'https://github.com/privacytoolsIO/privacy-tools',
+	'https://proton.me/blog'
+]);
