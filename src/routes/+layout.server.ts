2b8f992777a9b54bc080301cc93ea2a08b51fd80
jonathan <github.com.starship007@passmail.net>
2026-03-22 21:23:34 +0100

    add server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,12 @@
 import { error } from '@sveltejs/kit';
 
+import type { LayoutServerLoad } from './$types';
+
 import { rss } from '$lib/features/feed/service';
 import type { ArticlesResponse, SourceSearchParam } from '$lib/features/feed/types';
 import { tryCatch } from '$lib/utils/try-catch';
-import type { LayoutLoad } from './$types';
 
-export const ssr = false;
-
-export const load: LayoutLoad = async ({ url }) => {
+export const load: LayoutServerLoad = async ({ url }) => {
 	const sourceSearchParam = url.searchParams.get('source') as SourceSearchParam;
 
 	const { data, error: err } = await tryCatch(rss.getArticles({ source: sourceSearchParam }));
