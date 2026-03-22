24b09b155e749e153a77b966320b408d58de01b4
jonathan <github.com.starship007@passmail.net>
2026-03-22 12:59:20 +0100

    create about

---
+++ src/lib/components/about.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { Subheading } from './headings';
+	import { Text } from './text';
+
+	import { cn } from '$lib/utils/cn';
+
+	type AboutProps = HTMLAttributes<HTMLDivElement>;
+
+	let { class: klass, ...props }: AboutProps = $props();
+</script>
+
+<div class={cn('bg-base-300 p-4', klass)} {...props}>
+	<div>
+		<Subheading>About</Subheading>
+		<Text>10xPrivacy is a website</Text>
+	</div>
+</div>
