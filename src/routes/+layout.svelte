684a5edbe2915017d484db9d2c177dc7a533d181
jonathan <github.com.starship007@passmail.net>
2026-03-22 13:22:17 +0100

    use theme component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { resolve } from '$app/paths';
 	import LlmTermsOfServiceLink from '$lib/components/llm-terms-of-service-link.svelte';
 	import { Link } from '$lib/components/text';
+	import ThemeChange from '$lib/components/theme-change.svelte';
 	import '$lib/styles/app.css';
 
 	let { children } = $props();
@@ -32,36 +33,7 @@
 			</div>
 
 			<!-- theme toggle -->
-			<div class="dropdown dropdown-end ml-auto">
-				<div tabindex="0" role="button" class="btn gap-1 btn-ghost btn-sm">
-					<i class="nf nf-md-theme_light_dark"></i>
-					<svg
-						width="10px"
-						height="10px"
-						class="inline-block fill-current opacity-60"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 2048 2048"
-					>
-						<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-					</svg>
-				</div>
-				<ul
-					tabindex="-1"
-					class="dropdown-content z-50 mt-1 max-h-96 w-40 overflow-y-auto rounded-box bg-base-300 p-2 shadow-2xl"
-				>
-					{#each ['vscode', 'andromeda', 'ayudark', 'catppuccin', 'everforest', 'flexoki', 'githubdark', 'githublight', 'gruvbox', 'kanagawa', 'monokai', 'nightfox', 'nightowl', 'onedarkpro', 'rosepine', 'solarized', 'tokyonight'] as theme (theme)}
-						<li>
-							<input
-								type="radio"
-								name="theme-dropdown"
-								class="theme-controller btn btn-block w-full justify-start capitalize btn-ghost btn-xs"
-								aria-label={theme}
-								value={theme}
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
+			<ThemeChange />
 		</div>
 
 		<!-- mobile nav items  -->
