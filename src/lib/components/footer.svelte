970d855d9a14c68a2e88be16ff576b290a8c0931
jonathan <github.com.starship007@passmail.net>
2026-03-22 12:58:52 +0100

    create footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { Link, Text } from './text';
+
+	import { cn } from '$lib/utils/cn';
+
+	type FooterProps = HTMLAttributes<HTMLElement>;
+
+	let { class: klass, ...props }: FooterProps = $props();
+</script>
+
+<footer class={cn('w-full p-4 text-center', klass)} {...props}>
+	<Text>
+		© {new Date().getFullYear()} 10xPrivacy | Version: 1 Commit: asdasd123 |<Link
+			class="ml-2"
+			href="https://github.com/dottmp/10xprivacy"
+			external
+			variant="primary">GitHub</Link
+		>
+	</Text>
+</footer>
