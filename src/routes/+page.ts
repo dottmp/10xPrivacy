0bd01e9994cd6b004e48503d1ad500af97fe8d42
jonathan <github.com.starship007@passmail.net>
2026-03-22 21:31:46 +0100

    remove layout load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,11 @@
 import { error } from '@sveltejs/kit';
 
-import type { LayoutLoad } from './$types';
-
 import { rss } from '$lib/features/feed/service';
 import type { ArticlesResponse, SourceSearchParam } from '$lib/features/feed/types';
 import { tryCatch } from '$lib/utils/try-catch';
+import type { PageLoad } from './$types';
 
-export const load: LayoutLoad = async () => {
+export const load: PageLoad = async () => {
 	return {
 		articlesResponse: {
 			data: [],
