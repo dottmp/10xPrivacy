424acfd0640a10b05209dcb7799b98ffa4560c76
Jonathan <github.com.starship007@passmail.net>
2026-03-25 20:53:51 +0000

    Refactor/misc (#24)
    * fix formatting
    
    * add and remove comments
    
    * add default theme
    
    * add function too websites pages

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,6 @@
 declare const __COMMIT_HASH__: string;
 
 declare global {
-	//----------------------------------------------------------------------
-	// svelte internals
-	//----------------------------------------------------------------------
-
 	const __COMMIT_HASH__: string;
 
 	namespace App {
