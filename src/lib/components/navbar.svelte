1eebda29c5d232e1198826a126ccc44ab8e4fdee
jonathan <github.com.starship007@passmail.net>
2026-03-25 20:32:09 +0100

    add sidebar to navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -4,6 +4,8 @@
 	import { resolve } from '$app/paths';
 	import { Link } from '$lib/components/text';
 	import ThemeChange from '$lib/components/theme-change.svelte';
+
+	const DRAWER_ID = 'navbar-drawer';
 </script>
 
 <nav class="navbar flex-col justify-center gap-y-2 bg-base-100 px-4 shadow-sm">
@@ -17,23 +19,40 @@
 
 		<!-- desktop nav items  -->
 		<div class=" hidden flex-1 sm:block">
-			<ul class="flex space-x-6 px-6 [&_a]:font-semibold">
+			<ul
+				class="flex md:px-6 [&_a]:btn [&_a]:font-semibold [&_a]:btn-link [&_a]:no-underline [&_a]:hover:underline"
+			>
 				<li><Link href={resolve('/privacy-news')}>Privacy News</Link></li>
 				<li><Link href={resolve('/awesome-privacy')}>Awesome Privacy</Link></li>
 				<li><Link href={resolve('/websites')}>Websites</Link></li>
 			</ul>
 		</div>
 
-		<!-- theme toggle -->
-		<ThemeChange />
-	</div>
+		<!-- mobile nav items  -->
+		<div class="drawer drawer-end ml-auto w-fit sm:hidden">
+			<input id={DRAWER_ID} type="checkbox" class="drawer-toggle" />
+			<div class="drawer-content">
+				<label for={DRAWER_ID} class="drawer-button btn btn-square btn-ghost">
+					<i class="nf nf-md-menu text-lg"></i>
+				</label>
+			</div>
+			<div class="drawer-side">
+				<label for={DRAWER_ID} aria-label="close sidebar" class="drawer-overlay"></label>
+				<ul class="menu min-h-full w-80 bg-base-200 p-4">
+					<!-- Sidebar content here -->
+					<li><Link href={resolve('/privacy-news')}>Privacy News</Link></li>
+					<li><Link href={resolve('/awesome-privacy')}>Awesome Privacy</Link></li>
+					<li><Link href={resolve('/websites')}>Websites</Link></li>
+					<li class="mt-auto">
+						<ThemeChange variant="select" />
+					</li>
+				</ul>
+			</div>
+		</div>
 
-	<!-- mobile nav items  -->
-	<div class=" w-full flex-1 sm:hidden">
-		<ul class="flex flex-wrap gap-x-6 gap-y-2 [&_a]:font-semibold">
-			<li><Link href={resolve('/privacy-news')}>Privacy News</Link></li>
-			<li><Link href={resolve('/awesome-privacy')}>Awesome Privacy</Link></li>
-			<li><Link href={resolve('/websites')}>Websites</Link></li>
-		</ul>
+		<!-- theme toggle -->
+		<div class="hidden sm:block">
+			<ThemeChange variant="dropdown" />
+		</div>
 	</div>
 </nav>
