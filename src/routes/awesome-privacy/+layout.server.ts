a136ecc928cdf149d710101676f8670a1ee46794
Jonathan <github.com.starship007@passmail.net>
2026-03-25 19:52:18 +0000

    add cache controll to awesome privacy pages (#21)

--- src/routes/awesome-privacy/+layout.server.ts
+++ src/routes/awesome-privacy/+layout.server.ts
@@ -3,7 +3,11 @@ import type { LayoutServerLoad } from './$types';
 import { awesomePrivacy } from '$lib/features/awesome-privacy/service';
 import type { AwesomePrivacyData } from '$lib/features/awesome-privacy/types';
 
-export const load: LayoutServerLoad = () => {
+export const load: LayoutServerLoad = ({ setHeaders }) => {
+	setHeaders({
+		'cache-control': 'public, max-age=3600'
+	});
+
 	const awesomePrivacyData = awesomePrivacy.getData();
 
 	return {
