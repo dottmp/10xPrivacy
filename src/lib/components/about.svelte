9f4c1901a94c94ecf9e038a989971cf5de723c73
Jonathan <github.com.starship007@passmail.net>
2026-03-26 17:28:02 +0000

    Bugfix/typo in about (#25)
    * fix typo
    
    * move buttons to about

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -4,20 +4,27 @@
 	import { Subheading } from './headings';
 	import { Text } from './text';
 
+	import { resolve } from '$app/paths';
 	import { cn } from '$lib/utils/cn';
 
-	type AboutProps = HTMLAttributes<HTMLDivElement>;
+	type AboutProps = HTMLAttributes<HTMLElement>;
 
 	let { class: klass, ...props }: AboutProps = $props();
 </script>
 
-<div class={cn('rounded-lg bg-base-100 p-4 text-center', klass)} {...props}>
-	<div>
+<section class={cn('rounded-lg bg-base-100 p-4 text-center', klass)} {...props}>
+	<div class="flex flex-col">
 		<Subheading>About</Subheading>
 		<Text class="mt-2">
-			10xPrivacy is website dedicated to gathering and sharing information about digital privacy. It
-			provides a curated feed of news, resources, and tools to help users protect themselves from
-			surveillance capitalism.
+			10xPrivacy is a website dedicated to gathering and sharing information about digital privacy.
+			It provides a curated feed of news, resources, and tools to help users protect themselves from
+			surveillance capitalism. Start by going to one of the sections below.
 		</Text>
+
+		<div class="mx-auto mt-6 flex flex-col items-center gap-4 sm:flex-row">
+			<a class="btn btn-primary" href={resolve('/privacy-news')}> Privacy News </a>
+			<a class="btn btn-primary" href={resolve('/awesome-privacy')}> Awesome Privacy </a>
+			<a class="btn btn-primary" href={resolve('/websites')}> Websites </a>
+		</div>
 	</div>
-</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
 	import About from '$lib/components/about.svelte';
 	import Brand from '$lib/components/brand.svelte';
 </script>
 
 <main class="m-auto flex max-w-3xl flex-col items-center px-4 py-8">
 	<Brand size="lg" />
-
-	<div class="mx-auto mt-6 flex flex-col items-center gap-4 sm:mt-20 sm:flex-row">
-		<a class="btn btn-primary" href={resolve('/privacy-news')}> Privacy News </a>
-		<a class="btn btn-primary" href={resolve('/awesome-privacy')}> Awesome Privacy </a>
-		<a class="btn btn-primary" href={resolve('/websites')}> Websites </a>
-	</div>
 
 	<About class="mt-6 sm:mt-20" />
 </main>
