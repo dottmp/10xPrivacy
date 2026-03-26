1c247d7cb6022f75233d78a6bf76560bc8420ba7
jonathan <github.com.starship007@passmail.net>
2026-03-26 19:07:29 +0100

    move filtering to clientside

--- src/routes/privacy-news/+layout.server.ts
+++ src/routes/privacy-news/+layout.server.ts
@@ -3,13 +3,11 @@ import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 import { rss } from '$lib/features/feed/service';
-import type { ArticlesResponse, SourceSearchParam } from '$lib/features/feed/types';
+import type { ArticlesResponse } from '$lib/features/feed/types';
 import { tryCatch } from '$lib/utils/try-catch';
 
-export const load: LayoutServerLoad = async ({ url }) => {
-	const sourceSearchParam = url.searchParams.get('source') as SourceSearchParam;
-
-	const { data, error: err } = await tryCatch(rss.getArticles({ source: sourceSearchParam }));
+export const load: LayoutServerLoad = async () => {
+	const { data, error: err } = await tryCatch(rss.getArticles());
 
 	if (err) {
 		error(500, { message: 'Failed to fetch feed data' });
