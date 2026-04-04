b765c190801f047cae8b9dc42dbb06db0f90a3b1
Jonathan <github.com.starship007@passmail.net>
2026-04-04 19:36:55 +0000

    refactor: move configs to config file (#64)
    * refactor: move nav items config to configs file
    
    * refactor: move theme configs to config file
    
    * refactor: create theme and nav items configs in config file

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -5,23 +5,18 @@
 	import { Icons } from '$lib/components/icons/icons.svelte';
 	import { Link } from '$lib/components/text';
 	import ThemeChange from '$lib/components/theme-change.svelte';
+	import { NAV_ITMES } from '$lib/configs';
 
-	const DRAWER_ID = 'navbar-drawer';
+	const drawerId = 'navbar-drawer';
 
 	let drawer: HTMLInputElement | undefined;
 
 	function toggleDrawer() {
-		const drawer = document.getElementById(DRAWER_ID) as HTMLInputElement;
+		const drawer = document.getElementById(drawerId) as HTMLInputElement;
 		if (drawer) {
 			drawer.checked = !drawer.checked;
 		}
 	}
-
-	let navItems = [
-		{ href: resolve('/privacy-news'), label: 'Privacy News' },
-		{ href: resolve('/awesome-privacy'), label: 'Awesome Privacy' },
-		{ href: resolve('/websites'), label: 'Websites' }
-	];
 </script>
 
 <nav class="w-full bg-base-100 shadow-sm">
@@ -38,7 +33,7 @@
 				<ul
 					class="flex [&_a]:btn [&_a]:font-semibold [&_a]:btn-link [&_a]:no-underline [&_a]:hover:underline"
 				>
-					{#each navItems as navItem (navItem.href)}
+					{#each NAV_ITMES as navItem (navItem.href)}
 						<li><Link href={navItem.href}>{navItem.label}</Link></li>
 					{/each}
 				</ul>
@@ -46,17 +41,14 @@
 
 			<!-- mobile drawer -->
 			<div class="drawer drawer-end ml-auto w-fit sm:hidden">
-				<input bind:this={drawer} id={DRAWER_ID} type="checkbox" class="drawer-toggle" />
+				<input bind:this={drawer} id={drawerId} type="checkbox" class="drawer-toggle" />
 				<div class="drawer-content">
-					<label
-						for={DRAWER_ID}
-						class="drawer-button btn btn-square text-base-content/50 btn-ghost"
-					>
+					<label for={drawerId} class="drawer-button btn btn-square text-base-content/50 btn-ghost">
 						<Icons.menu class="text-lg" />
 					</label>
 				</div>
 				<div class="drawer-side">
-					<label for={DRAWER_ID} aria-label="close drawer" class="drawer-overlay"></label>
+					<label for={drawerId} aria-label="close drawer" class="drawer-overlay"></label>
 
 					<div class="flex min-h-full w-80 flex-col bg-base-200 *:w-full">
 						<div class="w-full bg-base-100 pt-2 pb-1">
@@ -66,7 +58,7 @@
 							</a>
 						</div>
 						<ul class="menu py-4">
-							{#each navItems as navItem (navItem.href)}
+							{#each NAV_ITMES as navItem (navItem.href)}
 								<li><Link href={navItem.href} onclick={toggleDrawer}>{navItem.label}</Link></li>
 							{/each}
 						</ul>
