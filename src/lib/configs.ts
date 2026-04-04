71010fdcde096545fd67ebe3892352c705456185
jonathan <github.com.starship007@passmail.net>
2026-04-04 21:33:14 +0200

    refactor: create theme and nav items configs in config file

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -1,3 +1,5 @@
+import { resolve } from '$app/paths';
+
 export const FEATURED_CATEGORIES = Object.freeze([
 	'Essentials',
 	'Communication',
@@ -6,3 +8,29 @@ export const FEATURED_CATEGORIES = Object.freeze([
 	'Operating Systems',
 	'Productivity'
 ]);
+
+export const THEMES = Object.freeze([
+	'vscode',
+	'andromeda',
+	'ayudark',
+	'catppuccin',
+	'everforest',
+	'flexoki',
+	'githubdark',
+	'githublight',
+	'gruvbox',
+	'kanagawa',
+	'monokai',
+	'nightfox',
+	'nightowl',
+	'onedarkpro',
+	'rosepine',
+	'solarized',
+	'tokyonight'
+] as const);
+
+export const NAV_ITMES = Object.freeze([
+	{ href: resolve('/privacy-news'), label: 'Privacy News' },
+	{ href: resolve('/awesome-privacy'), label: 'Awesome Privacy' },
+	{ href: resolve('/websites'), label: 'Websites' }
+] satisfies { href: string; label: string }[]);
