e2f1975c7c22679add1e1029f1178e2eb4febe14
jonathan <github.com.starship007@passmail.net>
2026-04-04 21:32:55 +0200

    refactor: move theme configs to config file

--- src/lib/components/theme-change.svelte
+++ src/lib/components/theme-change.svelte
@@ -4,6 +4,7 @@
 	import { themeChange } from 'theme-change';
 
 	import { Icons } from '$lib/components/icons/icons.svelte';
+	import { THEMES } from '$lib/configs';
 	import { cn } from '$lib/utils/cn';
 
 	type ThemeChangeProps = HTMLAttributes<HTMLDivElement> & {
@@ -11,26 +12,6 @@
 	};
 
 	let { variant = 'dropdown', class: klass, ...props }: ThemeChangeProps = $props();
-
-	const themes = [
-		'vscode',
-		'andromeda',
-		'ayudark',
-		'catppuccin',
-		'everforest',
-		'flexoki',
-		'githubdark',
-		'githublight',
-		'gruvbox',
-		'kanagawa',
-		'monokai',
-		'nightfox',
-		'nightowl',
-		'onedarkpro',
-		'rosepine',
-		'solarized',
-		'tokyonight'
-	];
 
 	onMount(() => {
 		themeChange(false);
@@ -57,7 +38,7 @@
 			tabindex="-1"
 			class="dropdown-content z-50 mt-1 max-h-96 w-40 overflow-y-auto rounded-box bg-base-300 p-2 shadow-2xl"
 		>
-			{#each themes as theme (theme)}
+			{#each THEMES as theme (theme)}
 				<li>
 					<input
 						type="radio"
@@ -73,7 +54,7 @@
 		</ul>
 	{:else}
 		<select data-choose-theme class="select appearance-none" aria-label="Select Theme">
-			{#each themes as theme (theme)}
+			{#each THEMES as theme (theme)}
 				<option class="theme-controller" aria-label={theme} value={theme}>{theme}</option>
 			{/each}
 		</select>
