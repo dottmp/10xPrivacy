a8ad3b0fcd0c57b012d1d45d66f3d2286e84bdd2
jonathan <github.com.starship007@passmail.net>
2026-04-06 15:36:52 +0200

    add cache control

--- src/routes/security/+page.server.ts
+++ src/routes/security/+page.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ locals }) => {
+export const load: PageServerLoad = ({ locals, setHeaders }) => {
+	setHeaders({ 'Cache-Control': 'public, max-age=1800' });
+
 	return {
 		headers: Object.entries(locals.securityHeaders).map(([name, value]) => ({ name, value }))
 	};
