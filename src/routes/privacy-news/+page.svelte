c3b1645dca4c0300ed3566fd0d8ec3c693b813d5
jonathan <github.com.starship007@passmail.net>
2026-04-25 01:20:07 +0200

    feat: update page description for privacy news

--- src/routes/privacy-news/+page.svelte
+++ src/routes/privacy-news/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Heading } from '$lib/components/headings/index.js';
-	import { Link, Text } from '$lib/components/text';
+	import { Text } from '$lib/components/text';
 	import ArticleFilters from '$lib/features/feed/components/article-filters.svelte';
 	import Articles from '$lib/features/feed/components/articles.svelte';
 
@@ -11,13 +11,8 @@
 	<header class="mb-8">
 		<Heading size="display" class="mb-4">Privacy News</Heading>
 		<Text>
-			RSS feed aggregator for privacy news from <Link
-				variant="primary"
-				href="https://tuta.com/blog"
-				external>tuta.com</Link
-			>, <Link variant="primary" href="https://www.privacyguides.org/blog/" external
-				>privacyguides.org</Link
-			>, and <Link variant="primary" href="https://techlore.tech/" external>techlore.tech</Link>.
+			RSS feed aggregator for privacy news from goated sources. Perfect read during your morning
+			coffee! ☕️
 		</Text>
 	</header>
 
