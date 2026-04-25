00e85a2e840fb32817aab8ac27bb419baf6f77a9
jonathan <github.com.starship007@passmail.net>
2026-04-26 00:00:52 +0200

    add new pages and support dropdown in navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import type { LayoutData } from '../../routes/$types';
 
+	import Subheading from './headings/subheading.svelte';
+
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
+	import { type Pathname } from '$app/types';
 	import { Icons } from '$lib/components/icons/icons.svelte';
-	import { Link } from '$lib/components/text';
 	import ThemeChange from '$lib/components/theme-change.svelte';
 	import { NAV_ITMES } from '$lib/configs';
 	import Search from '$lib/features/awesome-privacy/components/search.svelte';
@@ -36,7 +38,7 @@
 		<div class="flex w-full items-center">
 			<!-- logo -->
 			<a class="text-xl" href={resolve('/')}>
-				<Icons.logo size="sm" class="-ml-2" />
+				<Icons.logo size="sm" class="-ml-4" />
 				<span class="sr-only">Home</span>
 			</a>
 
@@ -72,11 +74,27 @@
 								</a>
 							</div>
 							<ul class="menu py-4">
-								{#each NAV_ITMES as navItem (navItem.href)}
-									<li><Link href={navItem.href} onclick={toggleDrawer}>{navItem.label}</Link></li>
-								{/each}
-							</ul>
+								{#snippet renderMobileNavItems(navItems: typeof NAV_ITMES)}
+									{#each navItems as navItem (navItem.href)}
+										<li class={cn()}>
+											{#if navItem.href}
+												<a href={resolve(navItem.href as Pathname)} onclick={toggleDrawer}
+													>{navItem.label}</a
+												>
+											{:else}
+												<Subheading class="mt-1 menu-title text-primary" size="sm">
+													{navItem.label}
+												</Subheading>
+												<ul class="ml-0 pl-0 before:hidden">
+													{@render renderMobileNavItems(navItem.subItems)}
+												</ul>
+											{/if}
+										</li>
+									{/each}
+								{/snippet}
 
+								{@render renderMobileNavItems(NAV_ITMES)}
+							</ul>
 							<div class="mt-auto w-full bg-base-100 py-4">
 								<ThemeChange variant="select" class=" w-full  px-4" />
 							</div>
@@ -85,14 +103,27 @@
 				</div>
 			</div>
 		</div>
-		<!-- desktop nav items  -->
-		<div class=" -mx-3 hidden md:block">
-			<ul
-				class="flex [&_a]:btn [&_a]:font-semibold [&_a]:btn-link [&_a]:no-underline [&_a]:hover:underline"
-			>
-				{#each NAV_ITMES as navItem (navItem.href)}
-					<li><Link href={navItem.href}>{navItem.label}</Link></li>
-				{/each}
+		<!-- desktop nav items -->
+		<div class="-mx-5 hidden md:block">
+			<ul class="menu menu-horizontal">
+				{#snippet renderNavItems(navItems: typeof NAV_ITMES)}
+					{#each navItems as navItem (navItem.href)}
+						<li>
+							{#if navItem.href}
+								<a href={resolve(navItem.href as Pathname)}>{navItem.label}</a>
+							{:else}
+								<details>
+									<summary>{navItem.label}</summary>
+									<ul class="min-w-40 rounded-t-none bg-base-100 p-2">
+										{@render renderNavItems(navItem.subItems)}
+									</ul>
+								</details>
+							{/if}
+						</li>
+					{/each}
+				{/snippet}
+
+				{@render renderNavItems(NAV_ITMES)}
 			</ul>
 		</div>
 	</div>
