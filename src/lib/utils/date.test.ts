ddacc8d202065be7aa8bc5a1e21432b2ec34319f
Jonathan <github.com.starship007@passmail.net>
2026-05-01 12:58:51 +0000

    fix: missing year for articles older than this year (#173)

--- src/lib/utils/date.test.ts
+++ src/lib/utils/date.test.ts
@@ -32,7 +32,11 @@ describe('formatDate', () => {
 		expect(formatDate('2026-01-16T08:00:00.000Z')).toBe('Jan 16');
 	});
 
-	it('returns formatted date with year for over 365 days ago', () => {
+	it('returns formatted date with year for a date in a previous year', () => {
 		expect(formatDate('2025-01-20T08:00:00.000Z')).toBe('Jan 20, 2025');
+	});
+
+	it('returns formatted date with year for Dec of last year (less than 365 days ago)', () => {
+		expect(formatDate('2025-12-20T08:00:00.000Z')).toBe('Dec 20, 2025');
 	});
 });
